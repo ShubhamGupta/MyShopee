0cc11c08a941c58a653cc044597200f410d94eb2
Aditya Raj <aditya@whizcreed.com>
2011-08-03 02:49:39 +0530

    Removed debug messages to console

--- source/core/mock.js
+++ source/core/mock.js
@@ -1,13 +1,13 @@
 var Mock = {
 	joinMember: function(){
             setTimeout(function(){
-                console.log("Mock.joinMember");
+                //console.log("Mock.joinMember");
                 Proxy.joinMember({id: 999, name: "Tin Tin", type: "phone", photo: "images/main/people/lisa_rogers.jpg"});
             }, 10000);
 	},
         
         joinMeeting: function(meeting){
-            console.log("Mock.joinMeeting");
+            //console.log("Mock.joinMeeting");
             setTimeout(function(){
                 Proxy.joinMeeting(meeting);
             }, 2500);
